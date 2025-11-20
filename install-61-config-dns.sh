bb47cabb86cdb23c4f77d3093cfd9054284dc56d
Gene @ asskick <gene@asskick.com>
2025-11-19 20:28:30 -0800

    add configs for dns and wireguard vpn

---
+++ install-61-config-dns.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+RESOLVCONF="/etc/resolv.conf"
+SYSTEMD_RESOLVCONF="/run/systemd/resolve/resolv.conf"
+
+# Configure DNS settings
+echo "Configuring DNS settings..."
+omarchy-setup-dns DHCP
+
+# /etc/resolv.conf needs to point to systemd-resolved's resolver
+if [[ -L "$RESOLVCONF" ]]; then
+    echo "$RESOLVCONF is already a link."
+else
+    echo "Linking $RESOLVCONF to $SYSTEMD_RESOLVCONF"
+    sudo rm "$RESOLVCONF"
+    sudo ln -s "$SYSTEMD_RESOLVCONF" "$RESOLVCONF"
+fi
---
+++ install-62-wireguard-vpn.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+NAME="WireGuard VPN"
+SCRIPT_URL="https://raw.githubusercontent.com/JacobusXIII/omarchy-wireguard-vpn-toggle/main/install.sh"
+INSTALL_CMD="curl -fsSL $SCRIPT_URL | bash"
+DIRECTORY="/etc/wireguard"
+COMMAND="resolvconf"
+LOCAL_BIN="/usr/local/bin/$COMMAND"
+LINK_TO="/usr/bin/resolvectl"
+
+# Install 
+echo "Installing $NAME..."
+($INSTALL_CMD)
+
+if [ ! -d "$DIRECTORY" ]; then
+    echo "$DIRECTORY does not exist, creating..."
+    sudo mkdir -p "$DIRECTORY"
+fi
+echo "$NAME config files need to be placed in $DIRECTORY"
+echo "$NAME installation complete!"
+
+# Check for command
+if ! command -v $COMMAND &> /dev/null; then
+    echo "$COMMAND command not found, linking to $LINK_TO..."
+    ln -s $LINK_TO $LOCAL_BIN
+else
+    echo "$COMMAND command found."
+fi
