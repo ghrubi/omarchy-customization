05e65ce4ff9b84a381187f1fbf85be06b2996406
Gene @ asskick <gene@asskick.com>
2025-11-27 17:01:36 -0800

    add config mega

--- install-03-megacmd.sh
+++ install-03-megacmd.sh
@@ -2,3 +2,17 @@
 
 # Install megacmd-bin from AUR
 yay -S --noconfirm --needed megacmd-bin fuse
+
+# Configure
+USERNAME="ghrubi@yahoo.com"
+MEGA_DIR="/home/gene/MEGA"
+LOCAL_DIR="/projects"
+
+# Prompt user to login to their MEGA account
+echo "Logging into Mega:"
+read -s -p "Enter your password for the next step: " user_password
+mega-login $USERNAME $user_password
+
+# Set up synchronization between local MEGA folder and local directory
+echo "Setting up synchronization between $MEGA_DIR and $LOCAL_DIR"
+mega-sync $MEGA_DIR $LOCAL_DIR
