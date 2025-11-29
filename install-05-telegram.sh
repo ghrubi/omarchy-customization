7a14f40b2c6e19d0b9ec66b2b71ec56cb3f3bbed
Gene @ asskick <gene@asskick.com>
2025-11-29 08:13:54 -0800

    remove telegram install hack

--- install-05-telegram.sh
+++ install-05-telegram.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
 # Install telegram-desktop from AUR
-# Hack due to telegram-desktop 404 in omarchy repo
-omarchy-refresh-pacman edge
 yay -S --noconfirm --needed telegram-desktop
-omarchy-refresh-pacman
 
